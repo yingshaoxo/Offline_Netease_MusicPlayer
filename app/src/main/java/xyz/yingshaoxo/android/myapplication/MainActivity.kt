74926f5da615d50ef216c14b3387861fbc246897
yingshaoxo <yingshaoxo@gmail.com>
2018-01-26 11:34:39 +0800

    fix error, now you can click the notification without problem.

--- app/src/main/java/xyz/yingshaoxo/android/myapplication/MainActivity.kt
+++ app/src/main/java/xyz/yingshaoxo/android/myapplication/MainActivity.kt
@@ -3,7 +3,6 @@ package xyz.yingshaoxo.android.myapplication
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
-import android.content.Intent
 import android.media.MediaPlayer
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -31,7 +30,7 @@ class MainActivity : AppCompatActivity() {
         mp = GlobalVariable.mp
         mp.isLooping = false
 
-        mp.setOnCompletionListener(MediaPlayer.OnCompletionListener {
+        mp.setOnCompletionListener({view ->
             this@MainActivity.finish()
         })
 
@@ -70,13 +69,13 @@ class MainActivity : AppCompatActivity() {
         var resultPendingIntent = PendingIntent.getActivity(
                 this,
                 0,
-                Intent(this, javaClass),
-                PendingIntent.FLAG_NO_CREATE
-        );
-        mBuilder.setContentIntent(resultPendingIntent);
+                intent,
+                PendingIntent.FLAG_UPDATE_CURRENT
+        )
+        mBuilder.setContentIntent(resultPendingIntent)
 
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        notificationManager.notify(0 /* ID of notification */, mBuilder.build())
+        notificationManager.notify(0, mBuilder.build())
 
         super.onBackPressed()
     }
